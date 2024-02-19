b0e4db2e2c372f5b0ae5831bf2a3eab0655c6b60
Tim Bytnar <tbytnar@gmail.com>
2024-02-18 18:09:29 -0600

    Better error handling and logging

--- meta_spotify_dj.py
+++ meta_spotify_dj.py
@@ -1,14 +1,19 @@
 import dxcam
 import pytesseract
-import cv2 
+import cv2
 import json
 import time
 import spotify_utilities as spotify
 import logging
 from datetime import datetime
 
-logging.basicConfig(filename="meta_spotify_dj.log", encoding="utf-8", level=logging.DEBUG)
-logging.info(f"\n\nMeta Spotify DJ Began Running @ {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}")
+logging.basicConfig(
+    filename="meta_spotify_dj.log", encoding="utf-8", level=logging.DEBUG, filemode="w"
+)
+logging.info(
+    f"\n\nMeta Spotify DJ Began Running @ {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
+)
+
 
 class Request:
     requestor = str
@@ -20,7 +25,7 @@ class Request:
 
     def __str__(self) -> str:
         return f"Request Object: Requestor = {self.requestor}, Track = {self.track}"
-    
+
 
 def CaptureScreen(camera):
     # img = np.array(Image.open("HW_DJ_Request_Test.png"))
@@ -33,21 +38,22 @@ def CaptureScreen(camera):
     # cv2.destroyAllWindows()
     return img
 
+
 def DetectChatWindows(img):
     # grayscale
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # threshold
-    ret,thresh = cv2.threshold(gray,65,255,0)
-    
+    ret, thresh = cv2.threshold(gray, 65, 255, 0)
+
     # Fill rectangular contours
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     for c in cnts:
-        cv2.drawContours(thresh, [c], -1, (255,255,255), -1)
+        cv2.drawContours(thresh, [c], -1, (255, 255, 255), -1)
 
     # Morph open
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
 
     # Draw rectangles, the 'area_treshold' value was determined empirically
@@ -57,8 +63,8 @@ def DetectChatWindows(img):
     area_max = 25000
     chats = []
     for c in cnts:
-        epsilon = 0.05*cv2.arcLength(c,True)
-        approx = cv2.approxPolyDP(c,epsilon,True)
+        epsilon = 0.05 * cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, epsilon, True)
         if len(approx) == 4:
             if cv2.contourArea(c) > area_min and cv2.contourArea(c) < area_max:
                 chats.append(cv2.boundingRect(c))
@@ -69,20 +75,20 @@ def DetectDJRequests(img, chats):
     current_requests = []
     for chat in chats:
         x, y, w, h = chat
-        cropped_img = img[y:y+h, x:x+w]
-        imgResized = cv2.resize(cropped_img, ( w*3, h*3))
-        #inverted_img = np.invert(cropped_img)
+        cropped_img = img[y : y + h, x : x + w]
+        imgResized = cv2.resize(cropped_img, (w * 3, h * 3))
+        # inverted_img = np.invert(cropped_img)
 
-        config = '--oem 3 --psm 11'
-        txt = pytesseract.image_to_string(imgResized, config = config, lang='eng')
+        config = "--oem 3 --psm 11"
+        txt = pytesseract.image_to_string(imgResized, config=config, lang="eng")
 
         if "djrequest:" in txt:
-            delimited_txt = txt.replace("\n\n", "|").replace("djrequest:","")
+            delimited_txt = txt.replace("\n\n", "|").replace("djrequest:", "")
             requestor = delimited_txt.split("|")[0]
             request_data = delimited_txt.split("|")[1:]
             requested_track = ""
             for segment in request_data:
-                requested_track += segment.replace("\n","") + " "
+                requested_track += segment.replace("\n", "") + " "
 
             new_request = Request(requestor, requested_track)
 
@@ -96,7 +102,7 @@ def DetectDJRequests(img, chats):
 
 # Tesseract Variables
 # NOTE: Tesseract (REQUIRED) for Windows can be downloaded here: https://github.com/UB-Mannheim/tesseract/wiki
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # DxCamera Variables
 dx_camera = dxcam.create()
@@ -110,33 +116,57 @@ while running:
     print("2 - Run DJ Bot")
     print("3 - Exit")
     menu_choice = input("What do you want to do?")
-    
+
     if menu_choice == "1":
         print("Welcome to the spotify device section!")
         spotify_manager.GetAndSetSpotifyDevice()
     if menu_choice == "2":
         print("The DJ Bot is running.  Press Control-C to exit from here.")
         while running:
-            spotify_manager.auth_manager, spotify_manager.spotify_connection = spotify_manager.Refresh_Spotify(spotify_manager.auth_manager, spotify_manager.spotify_connection)
+            spotify_manager.auth_manager, spotify_manager.spotify_connection = (
+                spotify_manager.Refresh_Spotify(
+                    spotify_manager.auth_manager, spotify_manager.spotify_connection
+                )
+            )
             screen_image = CaptureScreen(dx_camera)
             if screen_image is not None:
                 current_chats = DetectChatWindows(screen_image)
                 current_requests = DetectDJRequests(screen_image, current_chats)
                 for request in current_requests:
-                    buffer_search = next((x for x in requests_buffer if x.requestor == request.requestor and x.track == request.track), None)
+                    buffer_search = next(
+                        (
+                            x
+                            for x in requests_buffer
+                            if x.requestor == request.requestor
+                            and x.track == request.track
+                        ),
+                        None,
+                    )
                     if buffer_search is None:
                         logging.info(f"{request.requestor} requested: {request.track}")
                         print(f"{request.requestor} requested: {request.track}")
-                        results = spotify_manager.spotify_connection.search(request.track, 1, 0, "track") 
-                        songs_dict = results['tracks'] 
-                        song_items = songs_dict['items'] 
-                        song = song_items[0]['external_urls']['spotify'] 
-                        spotify_manager.spotify_connection.add_to_queue(song, spotify_manager.spotify_device_id)
+                        results = spotify_manager.spotify_connection.search(
+                            request.track, 1, 0, "track"
+                        )
+                        songs_dict = results["tracks"]
+                        song_items = songs_dict["items"]
+                        song = song_items[0]["external_urls"]["spotify"]
+                        spotify_manager.spotify_connection.add_to_queue(
+                            song, spotify_manager.spotify_device_id
+                        )
                         requests_buffer.append(request)
 
                 for request in requests_buffer:
-                    current_search = next((x for x in current_requests if x.requestor == request.requestor and x.track == request.track), None)
-                    if current_search is None: 
+                    current_search = next(
+                        (
+                            x
+                            for x in current_requests
+                            if x.requestor == request.requestor
+                            and x.track == request.track
+                        ),
+                        None,
+                    )
+                    if current_search is None:
                         requests_buffer.remove(request)
                     time.sleep(5)
             else:
--- spotify_utilities.py
+++ spotify_utilities.py
@@ -1,16 +1,34 @@
 import json
 import spotipy
+import logging
+
+logging.basicConfig(
+    filename="meta_spotify_dj.log", encoding="utf-8", level=logging.DEBUG, filemode="a"
+)
+
 
 def Create_Spotify(client_id, client_secret, redirect_uri):
-    auth_manager = spotipy.SpotifyOAuth(
-        scope=['playlist-modify-public','user-read-playback-state','user-modify-playback-state'],
-        redirect_uri=redirect_uri,
-        client_id=client_id,
-        client_secret=client_secret)
+    try:
+        auth_manager = spotipy.SpotifyOAuth(
+            scope=[
+                "playlist-modify-public",
+                "user-read-playback-state",
+                "user-modify-playback-state",
+            ],
+            redirect_uri=redirect_uri,
+            client_id=client_id,
+            client_secret=client_secret,
+        )
 
-    spotify = spotipy.Spotify(auth_manager=auth_manager)
+        spotify = spotipy.Spotify(auth_manager=auth_manager)
+        logging.info(f"Connected to Spotify with Client_ID: {client_id}")
+        return auth_manager, spotify
+    except:
+        print(
+            "Something went wrong authenticating to Spotify.  Please check your settings in dj_config.json and try again."
+        )
+        exit()
 
-    return auth_manager, spotify
 
 class SpotifyManager:
     client_id = str
@@ -27,30 +45,45 @@ class SpotifyManager:
             self.client_id = spotify_config.get("client_id")
             self.client_secret = spotify_config.get("client_secret")
             self.redirect_uri = spotify_config.get("redirect_uri")
-            self.auth_manager,self.spotify_connection = Create_Spotify(self.client_id, self.client_secret, self.redirect_uri)
+            self.auth_manager, self.spotify_connection = Create_Spotify(
+                self.client_id, self.client_secret, self.redirect_uri
+            )
             self.spotify_device_id = self.GetAndSetSpotifyDevice()
-            
+
     def Refresh_Spotify(self, authManager, spotify):
         token_info = authManager.cache_handler.get_cached_token()
         if authManager.is_token_expired(token_info):
-            authManager, spotify = Create_Spotify(self.client_id, self.client_secret, self.redirect_uri)
+            authManager, spotify = Create_Spotify(
+                self.client_id, self.client_secret, self.redirect_uri
+            )
         return authManager, spotify
 
     def GetAndSetSpotifyDevice(self):
-        user = self.spotify_connection.current_user() 
+        user = self.spotify_connection.current_user()
         devices = json.loads(json.dumps(self.spotify_connection.devices()))
         devices_json = devices.get("devices", None)
-        if len(devices_json) > 1:
-            print(f"Hello {user['display_name']}!  Your Spotify Devices:")
-            for n in range(0,len(devices_json)):
-                device_name = devices_json[n].get("name", None)
-                device_type = devices_json[n].get("type", None)
-                print(f"{n+1} = {device_name} - {device_type}")
+        if devices_json:
+            if len(devices_json) > 1:
+                print(f"Hello {user['display_name']}!  Your Spotify Devices:")
+                for n in range(0, len(devices_json)):
+                    device_name = devices_json[n].get("name", None)
+                    device_type = devices_json[n].get("type", None)
+                    print(f"{n+1} = {device_name} - {device_type}")
 
-            device_choice = int(input("Enter which device you want to send tracks to:")) - 1
-            device_id = devices_json[device_choice]['id']
+                device_choice = (
+                    int(input("Enter which device you want to send tracks to:")) - 1
+                )
+                device_id = devices_json[device_choice]["id"]
+            else:
+                print(
+                    f"Only one of your recently active devices was found.  Using it.  Info: {devices_json[0]['name']} "
+                )
+                device_id = devices_json[0]["id"]
         else:
-            print(f"Only one of your recently active devices was found.  Using it.  Info: {devices_json[0]['name']} ")
-            device_id = devices_json[0]['id']
+            print(
+                "No Spotify Clients are running.  Start one up and then restart the bot please."
+            )
+            exit()
 
+        print(device_id)
         return device_id
